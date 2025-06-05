e69ac3df70dd9713692d6eed2eeecb41dd901266
Ayush Soni <soni.ayush.2212@gmail.com>
2025-06-05 17:12:24 +0530

    Add a simple todo list

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class TodoScreen extends StatelessWidget {
+class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
+
+  @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  final TextEditingController _taskInputController = TextEditingController();
+
+  List<String> _todoList = [];
+
+  void _addTodo() {
+    if (_taskInputController.text.isNotEmpty) {
+      setState(() {
+        _todoList.add(_taskInputController.text);
+      });
+      _taskInputController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +28,64 @@ class TodoScreen extends StatelessWidget {
         title: const Text("Todo List"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(child: const Text("Todo List Screen")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Add a new task',
+                      hintText: 'Type here',
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _taskInputController,
+                  ),
+                ),
+                SizedBox(width: 8), // Add some spacing
+                ElevatedButton(
+                  onPressed: _addTodo,
+                  child: const Text('Add Todo'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_todoList[index]),
+                  leading: Icon(Icons.check),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        _todoList.removeAt(index);
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: Text('Menu'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
